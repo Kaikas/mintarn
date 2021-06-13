efadaf3d0c6ce149bd58fb701e9423b5e8476f91
Max Riechelmann <max.riechelmann@googlemail.com>
2021-06-13 23:51:41 +0200

    fix: script

--- scripts/sw_recode.sh
+++ scripts/sw_recode.sh
@@ -5,6 +5,6 @@ encoding=$(file -i "$2" | sed "s/.*charset=\(.*\)$/\1/")
 if [ ! "windows-1252" == "${encoding}" ]
 then
 	# Encodings differ, we have to encode
-	echo "recoding from ${encoding} to $1 file : $2"
+	echo "recoding from ${encoding} to "ANSI windows-1252" file : $2"
 	recode ${encoding}..windows-1252 ../$2
 fi
